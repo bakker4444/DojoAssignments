19136d7f2c12ca33ae7fcd83cf89fb0cfa5f5c88
Madin Kim <bakker4444@gmail.com>
2017-12-20 22:30:16 -0800

    bug fixed and changes the code for different approaches (reference on solution)

--- 001_WebFundamentals/0005_jQuery/contactcard/README.md
+++ 001_WebFundamentals/0005_jQuery/contactcard/README.md
@@ -11,5 +11,3 @@ verified by https://validator.w3.org/<br />
 <br />
 no error found<br />
 <br />
-[Still Working on assignment]<br />
-will add-on more features at the later<br />
--- 001_WebFundamentals/0005_jQuery/contactcard/contactcard.js
+++ 001_WebFundamentals/0005_jQuery/contactcard/contactcard.js
@@ -1,21 +1,18 @@
 $(document).ready(function(){
 
-    $(".namecard").click(
-        $(this).html(
-            "<p class='.namecard'>" + $("#desc").val() + "</p>"
-        )
-    )
-    
     $("form").submit(function(){
+        
         $(".shownamecard").append(
-            "<div class='namecard'><h1>" + $("#fname").val()+ " " + $("#lname").val() + "</h1><h3>Click for description!</h3><h4>" + $("#desc").val() + "</h4></div>"
+            "<div class='namecard'><h1>" + $("#fname").val() + " " + $("#lname").val() + "</h1><h3>Click for description!</h3><h4>" + $("#desc").val() + "</h4></div>"
         );
 
-        $(document).on("click", ".namecard", function(){
-            $(this).children().toggle();
-        });
+        $("#fname, #lname, #desc").val("");
 
         return false;
+    });
+
+    $(document).on('click', '.namecard', function(){
+        $(this).children().toggle();
     });
 
 });
