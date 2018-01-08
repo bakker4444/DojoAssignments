b303fb8fe497fa709e06babaf0fbacffe056fc71
Madin Kim <bakker4444@gmail.com>
2018-01-08 14:49:21 -0800

    upload Python / Multiples, Sum, Average assignment

---
+++ 002_Python/0001_Python/multiplesandsumandaverage/README.md
@@ -0,0 +1,9 @@
+## Python / "Multiples, Sum, Average" Assignment
+
+----
+
+**submit files:**<br />
+&nbsp;&nbsp;&nbsp;&nbsp;multiples_sum_average.py<br />
+<br />
+Python version : 2.7.14<br />
+<br />
---
+++ 002_Python/0001_Python/multiplesandsumandaverage/multiples_sum_average.py
@@ -0,0 +1,19 @@
+# Multiples
+# Part 1 - print odd numbers from 1 to 1000
+for i in range(1, 1001, 2):
+    print i
+
+# Multiples
+# Part 2 - print all the multiples of 5 from 5 to 1,000,000
+for i in range(5, 1000001, 5):
+    print i
+
+# Sum List
+# Create a program that prints the sum of all the values in the list
+a = [1, 2, 5, 10, 255, 3]
+print sum(a)
+
+# Average Lists
+# Create a program that prints the average of the values in the list
+b = [1, 2, 5, 10, 255, 3]
+print sum(b)/len(b)
