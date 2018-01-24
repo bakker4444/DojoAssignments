5296a751452916d52a6b7dad83e98c433472546e
Madin Kim <bakker4444@gmail.com>
2018-01-23 21:24:14 -0800

    code organized

--- 002_Python/0006_Django/session_words/apps/session_words_app/urls.py
+++ 002_Python/0006_Django/session_words/apps/session_words_app/urls.py
@@ -4,9 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^session_words$', views.index),
-
     url(r'^session_words/process$', views.process),
-    
     url(r'^session_words/reset$', views.reset),
     url(r'^reset$', views.reset),
 ]
