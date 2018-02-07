d25b57f1d6ac2ea273b3aab8e8346d899d40d710
Madin Kim <bakker4444@gmail.com>
2018-02-07 15:18:38 -0800

    upload Node / Landing Page Assignment

---
+++ 003_MEAN/0002_Node_v3/landing_page/README.md
@@ -0,0 +1,20 @@
+## Node / "Landging Page" Assignment
+
+----
+
+**submit files:**<br />
+
+```
+    .
+    ├── app.js
+    ├── dojo.html
+    ├── index.html
+    └── ninjas.html
+
+```
+
+<br />
+Node version : 8.9.4.<br />
+NodeMon version : 1.14.12.<br />
+<br />
+
---
+++ 003_MEAN/0002_Node_v3/landing_page/app.js
@@ -0,0 +1,34 @@
+var http = require("http");
+var fs = require("fs");
+
+var server = http.createServer(function (request, response) {
+    console.log("client request URL: ", request.url);
+
+    if (request.url === "/") {
+        fs.readFile("index.html", "utf8", function(errors, contents){
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if (request.url === "/ninjas") {
+        fs.readFile("ninjas.html", "utf8", function(errors, contents) {
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if (request.url === "/dojos/new"){
+        fs.readFile("dojo.html", "utf8", function(errors, contents){
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else {
+        response.writeHead(404);
+        response.end("File not found!!!");
+    }
+});
+server.listen(6789);
+console.log("Running in localhose at port 6789")
