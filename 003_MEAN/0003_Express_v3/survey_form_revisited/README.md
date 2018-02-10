7b7eeaaedef890177c4224db778280f24b600400
Madin Kim <bakker4444@gmail.com>
2018-02-09 17:24:27 -0800

    upload Express / Survey Form Revisited Assignment

---
+++ 003_MEAN/0003_Express_v3/survey_form_revisited/README.md
@@ -0,0 +1,28 @@
+## Express / "Survey Form Revisited" Assignment
+
+----
+
+**submit files:**<br />
+
+```
+    .
+    ├── server.js
+    ├── package.json
+    ├── package-lock.json
+    ├── static/
+    │   └── index.css
+    └── views/
+        └── index.ejs
+
+```
+
+<br />
+<version>
+Node : 8.9.4<br />
+NodeMon : 1.14.12<br />
+Express : 4.16.2<br />
+Ejs : 2.5.7<br />
+morgan : 1.9.0<br />
+jQuery : 3.3.1<br />
+<br />
+
---
+++ 003_MEAN/0003_Express_v3/survey_form_revisited/server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+var port = 5000;
+var path = require("path");
+var morgan = require("morgan");
+
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./static")));
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "ejs");
+app.get("/", function (request, response) {
+    response.render("index");
+});
+
+var server = app.listen(port, function () {
+    console.log("listening on port " + port);
+});
+
+var io = require("socket.io").listen(server);
+
+io.sockets.on("connection", function (socket) {
+    console.log("Client/socket is connected!");
+    console.log("Client/socket id is : ", socket.id);
+
+    socket.on("posting_form", function (data) {
+        console.log(data);
+        socket.emit("updated_message", {
+            message_from_user: "You emitted the following information to the server: { name: " + data.name + ", location: " + data.dojo_location + ", language: " + data.fav_language + ", comment: " + data.comment + " }",
+            message_random_num: "Your lucky number emitted by the server is " + (Math.floor(Math.random() * 1000) + 1)
+        });
+    });
+    
+});
