7a371b144f3e658f6b0f1b93f6f709a09377c6a3
Madin Kim <bakker4444@gmail.com>
2018-02-22 17:37:40 -0800

    formatting

--- 003_MEAN/0006_Angular/authors/client/src/app/new/new.component.ts
+++ 003_MEAN/0006_Angular/authors/client/src/app/new/new.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from "./../http.service";
 
-import { Router} from "@angular/router";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-new',
