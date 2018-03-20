6452fd0dda9d423ce229971ca59dad25cdb6b428
Madin Kim <bakker4444@gmail.com>
2018-03-19 17:31:06 -0700

    upload iOS / iOS Advanced / GET Films Assignment

---
+++ 004_iOS/0005_iOS_Advanced/get_films/Star Wars Encyclopedia/Star Wars Encyclopedia/FilmViewController.swift
@@ -0,0 +1,105 @@
+//
+//  FilmTableViewController.swift
+//  Star Wars Encyclopedia
+//
+//  Created by Madin Kim on 3/19/18.
+//  Copyright © 2018 DINDIN. All rights reserved.
+//
+
+import UIKit
+
+class FilmViewController: UITableViewController {
+    
+    var films:[String] = []
+    
+    func dataLoading(urlInput: String) {
+        
+        // specify the url that we will be sending the GET Request to
+        let url = URL(string: urlInput)
+        // create a URLSession to handle the request tasks
+        let session = URLSession.shared
+        // create a "data task" to make the request and run the completion handler
+        
+        let task = session.dataTask(with: url!, completionHandler: {
+            // see: Swift closure expression syntax
+            data, response, error in
+            // data -> JSON data, response -> headers and other meta-information, error-> if one occurred
+            // "do-try-catch" blocks execute a try statement and then use the catch statement for errors
+            do {
+                // try converting the JSON object to "Foundation Types" (NSDictionary, NSArray, NSString, etc.)
+                if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
+                    // Why do we need to optionally unwrap jsonResult["results"]
+                    // Try it without the optional unwrapping and you'll see that the value is actually an optional
+                    if let results = jsonResult["results"] {
+                        // coercing the results object as an NSArray and then storing that in resultsArray
+                        let resultsArray = results as! NSArray
+                        // now we can run NSArray methods like count and firstObject
+                        
+                        // appending names from JSON data to films array
+                        for i in 0..<resultsArray.count {
+//                            print(resultsArray[i])
+                            let arr1 = resultsArray[i] as! NSDictionary
+                            let title = arr1["title"] as! NSString
+                            self.films.append(title as String)
+                        }
+                        
+                        // Dispatching from upper thread to main thread as asyncronous execution
+                        DispatchQueue.main.async{
+                            self.tableView.reloadData()
+                        }
+                    }
+                    
+                    // if next key is exists, run dataLoading function again with new url
+                    // (using recursion)
+                    if let nextPage = jsonResult["next"] {
+                        if nextPage is NSNull {
+                            print(nextPage)
+                        } else {
+                            print(nextPage)
+                            self.dataLoading(urlInput: nextPage as! String)
+                        }
+                    } else {
+                        print("\(error ?? "No next page key" as! Error)")
+                    }
+                }
+            } catch {
+                print(error)
+            }
+        })
+        // execute the task and wait for the response before
+        // running the completion handler. This is async!
+        task.resume()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dataLoading(urlInput: "http://swapi.co/api/films")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // if we return - sections we won't have any sections to put our rows in
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // return the count of films in our data array
+        return films.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create a generic cell
+        let cell = UITableViewCell()
+        // set the default cell label to the corresponding element in the films array
+        cell.textLabel?.text = films[indexPath.row]
+        // return the cell so that it can be rendered
+        return cell
+    }
+    
+}
+
+
