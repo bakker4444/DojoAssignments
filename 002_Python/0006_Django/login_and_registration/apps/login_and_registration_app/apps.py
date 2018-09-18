c81b79500e8f18d0b058ab74b268747f842e59c4
bakker4444 <bakker4444@gmail.com>
2018-09-17 19:18:27 -0700

    make separate repo

--- 002_Python/0006_Django/login_and_registration/apps/login_and_registration_app/apps.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class LoginAndRegistrationAppConfig(AppConfig):
-    name = 'login_and_registration_app'
